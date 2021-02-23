ba4b5838366c4ce25c2186d05bbf5e733b516bac
Goksal TAK <goksaltak@hotmail.com>
2021-02-23 23:31:52 +0300

    Add project files.

---
+++ My_Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace My_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> kisiler = new MyList<string>();
+            kisiler.Add ("Göksal");
+            kisiler.Add("Gökçe");
+            kisiler.Add("Hatice");
+
+            foreach (var isim in kisiler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+        }
+    }
+}
